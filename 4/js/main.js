6556efb047ca6fc0c070421e8e6fea142349650b
Keks <keks@htmlacademy.ru>
2022-07-20 15:22:15 +0300

    :heavy_check_mark: Сборка #4

--- 4/js/main.js
+++ 4/js/main.js
@@ -1,20 +1,17 @@
+const MAXLENGTH = 140
+
 // https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript Источник генератора чисел.
 
-let randomNumber = function(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min)
-  }
-  
-  console.log(randomNumber(1, 30));
+function getRandomNumber (min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+console.log(getRandomNumber(1, 30))
 
 // https://learn.javascript.ru/string#:~:text=Создайте%20функцию%20truncate(str%2C%20maxlength)%2C%20которая,её%20длина%20стала%20равна%20maxlength источник проверки.
 
-let maxLength = 140;
-
-let getCheckingTheQuantity = function(numberOfLine, maxLength) {
-    if (numberOfLine.length > maxLength) {
-        return false;
-    } 
-    return true;
+function checkStringLength (numberOfLine, MAXLENGTH) {
+  return (numberOfLine.length < MAXLENGTH)
 }
 
-console.log(getCheckingTheQuantity(1, maxLength));
+console.log(checkStringLength(1, MAXLENGTH))

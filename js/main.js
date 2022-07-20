d1c01be0f67328301cfca4ab6848889e3a5a482c
Nexoner <101067653+Nexoner@users.noreply.github.com>
2022-07-20 15:21:47 +0300

    edit

--- js/main.js
+++ js/main.js
@@ -1,18 +1,17 @@
-const MAXLENGTH = 140;
+const MAXLENGTH = 140
 
 // https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript Источник генератора чисел.
 
+function getRandomNumber (min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
 
-let getRandomNumber = function(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min)
-  }
-  
-  console.log(getRandomNumber(1, 30));
+console.log(getRandomNumber(1, 30))
 
 // https://learn.javascript.ru/string#:~:text=Создайте%20функцию%20truncate(str%2C%20maxlength)%2C%20которая,её%20длина%20стала%20равна%20maxlength источник проверки.
 
-let checkStringLength = function(numberOfLine, MAXLENGTH) {
-    return (numberOfLine.length < MAXLENGTH) ? true : false;
+function checkStringLength (numberOfLine, MAXLENGTH) {
+  return (numberOfLine.length < MAXLENGTH)
 }
 
-console.log(checkStringLength(1, MAXLENGTH));
+console.log(checkStringLength(1, MAXLENGTH))

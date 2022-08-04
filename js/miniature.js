ac9753790328edd7e9b1f3dc38f8ac187abd53ba
Эрнест <101067653+Nexoner@users.noreply.github.com>
2022-08-04 08:49:22 +0300

    Update miniature.js

--- js/miniature.js
+++ js/miniature.js
@@ -6,11 +6,11 @@ userVisibleMiniature.classList.remove('visually-hidden');
 
 const NEW_PICTURE_TEMPLATE = document.querySelector('#picture')
   .content
-  .querySelector('.pictures');
+  .querySelector('.picture');
 const PHOTOS = [];
 const pictures = document.querySelector('.pictures');
 
-for (let i = 0; i < NEED_MASSIVES; i++)  {
+for (let i = 1; i <= NEED_MASSIVES; i++)  {
   PHOTOS.push(getPhotoInfo(i));
 }
 
@@ -20,7 +20,7 @@ PHOTOS.forEach (({url, likes, comments}) => {
   const newPicture = NEW_PICTURE_TEMPLATE.cloneNode(true);
   newPicture.querySelector('.picture__img').src = url;
   newPicture.querySelector('.picture__comments').textContent = comments.length;
-  newPicture.querySelector('..picture__likes').textContent = likes;
+  newPicture.querySelector('.picture__likes').textContent = likes;
 
   photoFragment.append(newPicture);
 });
